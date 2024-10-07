50ba470dc93e7a948693efc6c3092a9bd1391a3f
gaurav-sharma6338 <gaurav.sharma6338@gmail.com>
2024-10-07 18:45:08 +0530

    Modified HTML and CSS File

--- Weather App Project/script.js
+++ Weather App Project/script.js
@@ -4,7 +4,7 @@ const locationButton = document.querySelector(".location-btn");
 const currentWeatherDiv = document.querySelector(".current-weather");
 const weatherCardsDiv = document.querySelector(".weather-cards");
 
-const API_KEY = "YOUR-API-KEY-HERE"; // API key for OpenWeatherMap API
+const API_KEY = "d331a74c8fb877d81b72ee3bf20c9790"; // API key for OpenWeatherMap API
 
 const createWeatherCard = (cityName, weatherItem, index) => {
     if(index === 0) { // HTML for the main weather card
